9f2a7295236a9c15d5fe3c77ff49f79810acee9f
sebehhhhhh <muhammad.seman030801@gmail.com>
2024-06-08 20:48:57 -0700

    Your commit message

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ Route::middleware(['guest'])->group(function () {
 Route::middleware(['auth'])->group(function () {
     Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
-    Route::prefix('dashboard')->name('dashboard.')->group(function () {
+    Route::prefix('dashboard/')->name('dashboard.')->group(function () {
         Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
         Route::get('/transaction-chart', [DashboardController::class, 'getTransactionChartData'])->name('chart');
 
